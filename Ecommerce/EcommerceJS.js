96c64aef9f2229d32b20ddd151fd5b9412b0396c
limjianntzer0134@outlook.com <limjianntzer0134@outlook.com>
2020-03-14 14:53:13 +0800

    Comepleted

--- Ecommerce/EcommerceJS.js
+++ Ecommerce/EcommerceJS.js
@@ -1,5 +1,9 @@
 ﻿var cartNo = "";
 var cart = [];
+var subtotal = 0;
+var itemtotal = 0;
+var shipfee = 10;
+var total = 0;
 
 var LGMouse = 45;
 var LGKeyboard = 50;
@@ -18,6 +22,12 @@ var HPKeyboardClicked = false;
 
 var MicrosoftMouseClicked = false;
 var MicrosoftKeyboardClicked = false;
+
+var ClearAppear = false;
+
+var cartLength = false;
+
+var ShipFeeIncluded = false;
 
 var LGmousetotal = 1;
 var LGmouseclick = 1;
@@ -80,10 +90,95 @@ const MicrosoftKBcal = () => {
     $("#MicrosoftKBprice").html(MicrosoftKeyboard);
 }
 
+const ClearHtml = () => {
+    debugger
+    if (ClearAppear == false) {
+        $("#ClearAll").html("Clear Cart");
+        ClearAppear = true;
+    }
+}
+
+const ClearCart = () => {
+    debugger
+    $("#OrderSummary").html("");
+    $("#cartList").html("");
+    $("#cartTotal").html(0);
+    $("#cartList").append("<button id=ClearAll>");
+    LGMouseClicked = false;
+    LGKeyboardClicked = false;
+    HPMouseClicked = false;
+    HPKeyboardClicked = false;
+    MicrosoftMouseClicked = false;
+    MicrosoftKeyboardClicked = false;
+    ClearAppear = false;
+    cartLength = false;
+    ShipFeeIncluded = false;
+    cart = [];
+    subtotal = 0;
+    itemtotal = 0;
+    total = 0;
+
+    LGmouseclick = 1;
+    LGkeyboardclick = 1;
+    HPmouseclick = 1;
+    HPkeyboardclick = 1;
+    Microsoftmouseclick = 1;
+    Microsoftkeyboardclick = 1;
+
+    LGmousetotal = 1;
+    LGkeyboardtotal = 1;
+    HPmousetotal = 1;
+    HPkeyboardtotal = 1;
+    Microsoftmousetotal = 1;
+    Microsoftkeyboardtotal = 1;
+
+    LGMouse = 45;
+    LGKeyboard = 50;
+    HPMouse = 35;
+    HPKeyboard = 32;
+    MicrosoftMouse = 43;
+    MicrosoftKeyboard = 39;
+}
+
+const OrderSummary = () => {
+    debugger
+    if (cartLength == false) {
+        if (cart.length != 0) {
+            $("#OrderSummary").append("<br/>");
+            $("#OrderSummary").append("<h4>Order Summary</h4>");
+            $("#OrderSummary").append("<button class=text>Subtotal</button>");
+            $("#OrderSummary").append("<button class=text>(</button>");
+            $("#OrderSummary").append("<button id=item></button>");
+            $("#OrderSummary").append("<button class=text>item)</button>");
+            $("#OrderSummary").append("<button id=SubTotal></button>");
+            $("#OrderSummary").append("<br/>");
+            $("#OrderSummary").append("<button class=text>Shipping Fee</button>");
+            $("#OrderSummary").append("<button id=ShipFee></button>");
+            $("#OrderSummary").append("<br/>");
+            $("#OrderSummary").append("<button class=text>Total</button>");
+            $("#OrderSummary").append("<button id=Total></button>");
+
+            $("#item").html(itemtotal);
+            $("#SubTotal").html(subtotal);
+            $("#ShipFee").html(shipfee);
+            total = total + shipfee;
+            $("#Total").html(total);
+            cartLength = true;
+        }
+    }
+    else {
+        $("#item").html(itemtotal);
+        $("#SubTotal").html(subtotal);
+        $("#ShipFee").html(shipfee);
+        $("#Total").html(total);
+    }
+}
+
 
 $(document).ready(function () {
 
     $("#LogitechMouse").click(function (num) {
+        debugger
         if (LGMouseClicked == false) {
             increaseOrder("+" + num.target.value);
             LGMouseClicked = true;
@@ -91,28 +186,41 @@ $(document).ready(function () {
         DisplayResult();
 
         if (LGmouseclick == 1) {
+
             var chartlist = $("<div></div>").text("Logitech Mouse 45.00 ");
 
             $(chartlist).append("<button id=mouseplus value=+>+</button>");
             $(chartlist).append("<button id=mousetotal value=1>1</button>");
             $(chartlist).append("<button id=mouseminus value=->-</button>");
-            $(chartlist).append("<button id=mouseprice>45.00</>");
+            $(chartlist).append("<button id=mouseprice>45.00</button>");
             $(chartlist).append("<button id=mousedel>x</button>");
 
             $("#cartList").append(chartlist);
-            LGmouseclick += 1;
+            LGmouseclick += 1;  
+            subtotal += 45;
+            itemtotal += 1;
+            total += 45;
 
             $("#mouseplus").click(function () {
+                debugger
                 LGmousetotal += 1;
+                itemtotal += 1;
                 LGMouse += 45;
+                subtotal += 45;
+                total += 45;
                 LGMouseCal();
+                OrderSummary();
             })
 
             $("#mouseminus").click(function () {
                 if ($("#mousetotal").html() != 1) {
                     LGmousetotal -= 1;
+                    itemtotal -= 1;
                     LGMouse -= 45;
+                    subtotal -= 45;
+                    total -= 45;
                     LGMouseCal();
+                    OrderSummary();
                 }
                 else {
                     $("#mousetotal").html(1);
@@ -120,7 +228,6 @@ $(document).ready(function () {
             })
 
             $("#mousedel").click(function () {
-                debugger
                 $(this).parent().remove();
                 LGMouseClicked = false;
                 LGmouseclick = 1;
@@ -132,12 +239,21 @@ $(document).ready(function () {
                     DisplayResult();
                 }
             })
+
+            $("#ClearAll").click(function () {
+                ClearCart();
+            })
         }
         else {
             LGmousetotal += 1;
+            itemtotal += 1;
             LGMouse += 45;
+            subtotal += 45;
+            total += 45;
             LGMouseCal();
         }
+        ClearHtml();
+        OrderSummary();
     })
 
     $("#LogitechKeyboard").click(function (num) {
@@ -159,18 +275,29 @@ $(document).ready(function () {
 
             $("#cartList").append(chartlist);
             LGkeyboardclick += 1;
+            subtotal += 50;
+            itemtotal += 1;
+            total += 50;
 
             $("#KBplus").click(function () {
                 LGkeyboardtotal += 1;
                 LGKeyboard += 50;
+                subtotal += 50;
+                total += 50;
+                itemtotal += 1;
                 LGKBCal();
+                OrderSummary();
             })
 
             $("#KBminus").click(function () {
                 if ($("#KBtotal").html() != 1) {
                     LGkeyboardtotal -= 1;
                     LGKeyboard -= 50;
+                    subtotal -= 50;
+                    itemtotal -= 1;
+                    total -= 50;
                     LGKBCal();
+                    OrderSummary();
                 }
                 else {
                     $("#KBtotal").html(1);
@@ -189,12 +316,21 @@ $(document).ready(function () {
                     DisplayResult();
                 }
             })
+
+            $("#ClearAll").click(function () {
+                ClearCart();
+            })
         }
         else {
             LGkeyboardtotal += 1;
             LGKeyboard += 50;
+            subtotal += 50;
+            itemtotal += 1;
+            total += 50;
             LGKBCal();
         }
+        ClearHtml();
+        OrderSummary();
     })
 
     $("#HPMouse").click(function (num) {
@@ -216,18 +352,29 @@ $(document).ready(function () {
 
             $("#cartList").append(chartlist);
             HPmouseclick += 1;
+            subtotal += 35;
+            itemtotal += 1;
+            total += 35;
 
             $("#HPMplus").click(function () {
                 HPmousetotal += 1;
                 HPMouse += 35;
+                subtotal += 35;
+                itemtotal += 1;
+                total += 35;
                 HPMousecal();
+                OrderSummary();
             })
 
             $("#HPMminus").click(function () {
                 if ($("#HPMtotal").html() != 1) {
                     HPmousetotal -= 1;
                     HPMouse -= 35;
+                    subtotal -= 35;
+                    itemtotal -= 1;
+                    total -= 35;
                     HPMousecal();
+                    OrderSummary();
                 }
                 else {
                     $("#HPMtotal").html(1);
@@ -246,12 +393,20 @@ $(document).ready(function () {
                     DisplayResult();
                 }
             })
+            $("#ClearAll").click(function () {
+                ClearCart();
+            })
         }
         else {
             HPmousetotal += 1;
             HPMouse += 35;
+            subtotal += 35;
+            itemtotal += 1;
+            total += 35;
             HPMousecal();
         }
+        ClearHtml();
+        OrderSummary();
     })
 
     $("#HPKeyboard").click(function (num) {
@@ -273,18 +428,29 @@ $(document).ready(function () {
 
             $("#cartList").append(chartlist);
             HPkeyboardclick += 1;
+            subtotal += 32;
+            itemtotal += 1;
+            total += 32;
 
             $("#HPKBplus").click(function () {
                 HPkeyboardtotal += 1;
                 HPKeyboard += 32;
+                subtotal += 32;
+                itemtotal += 1;
+                total += 32;
                 HPKBcal();
+                OrderSummary();
             })
 
             $("#HPKBminus").click(function () {
                 if ($("#HPKBtotal").html() != 1) {
                     HPkeyboardtotal -= 1;
                     HPKeyboard -= 32;
+                    subtotal -= 32;
+                    itemtotal -= 1;
+                    total -= 32;
                     HPKBcal();
+                    OrderSummary();
                 }
                 else {
                     $("#HPKBtotal").html(1);
@@ -303,12 +469,20 @@ $(document).ready(function () {
                     DisplayResult();
                 }
             })
+            $("#ClearAll").click(function () {
+                ClearCart();
+            })
         }
         else {
             HPkeyboardtotal += 1;
             HPKeyboard += 32;
+            subtotal += 32;
+            itemtotal += 1;
+            total += 32;
             HPKBcal();
         }
+        ClearHtml();
+        OrderSummary();
     })
 
     $("#MicrosoftMouse").click(function (num) {
@@ -330,18 +504,29 @@ $(document).ready(function () {
 
             $("#cartList").append(chartlist);
             Microsoftmouseclick += 1;
+            subtotal += 43;
+            itemtotal += 1;
+            total += 43;
 
             $("#MicrosoftMplus").click(function () {
                 Microsoftmousetotal += 1;
                 MicrosoftMouse += 43;
+                subtotal += 43;
+                itemtotal += 1;
+                total += 43;
                 MicrosoftMousecal();
+                OrderSummary();
             })
 
             $("#MicrosoftMminus").click(function () {
                 if ($("#MicrosoftMtotal").html() != 1) {
                     Microsoftmousetotal -= 1;
                     MicrosoftMouse -= 43;
+                    subtotal -= 43;
+                    itemtotal -= 1;
+                    total -= 43;
                     MicrosoftMousecal();
+                    OrderSummary();
                 }
                 else {
                     $("#MicrosoftMtotal").html(1);
@@ -360,12 +545,20 @@ $(document).ready(function () {
                     DisplayResult();
                 }
             })
+            $("#ClearAll").click(function () {
+                ClearCart();
+            })
         }
         else {
             Microsoftmousetotal += 1;
             MicrosoftMouse += 43;
+            subtotal += 43;
+            itemtotal += 1;
+            total += 43;
             MicrosoftMousecal();
         }
+        ClearHtml();
+        OrderSummary();
     })
 
     $("#MicrosoftKeyboard").click(function (num) {
@@ -386,21 +579,32 @@ $(document).ready(function () {
             $(chartlist).append("<button id=MicrosoftKBminus value=->-</button>");
             $(chartlist).append("<button id=MicrosoftKBprice>39.00</button>");
             $(chartlist).append("<button id=MicrosoftKBdel>x</button>");
-
+            
             $("#cartList").append(chartlist);
             Microsoftkeyboardclick += 1;
+            subtotal += 39;
+            itemtotal += 1;
+            total += 39;
 
             $("#MicrosoftKBplus").click(function () {
                 Microsoftkeyboardtotal += 1;
                 MicrosoftKeyboard += 39;
+                subtotal += 39;
+                itemtotal += 1;
+                total += 39;
                 MicrosoftKBcal();
+                OrderSummary();
             })
 
             $("#MicrosoftKBminus").click(function () {
                 if ($("#MicrosoftKBtotal").html() != 1) {
                     Microsoftkeyboardtotal -= 1;
                     MicrosoftKeyboard -= 39;
+                    subtotal -= 39;
+                    itemtotal -= 1;
+                    total -= 39;
                     MicrosoftKBcal();
+                    OrderSummary();
                 }
                 else {
                     $("#MicrosoftKBtotal").html(1);
@@ -419,11 +623,25 @@ $(document).ready(function () {
                     DisplayResult();
                 }
             })
+            $("#ClearAll").click(function () {
+                ClearCart();
+            })
         }
         else {
             Microsoftkeyboardtotal += 1;
             MicrosoftKeyboard += 39;
+            subtotal += 39;
+            itemtotal += 1;
+            total += 39;
             MicrosoftKBcal();
         }
+        ClearHtml();
+        OrderSummary();
     })
+
+    $("#ClearAll").click(function () {
+        debugger
+        ClearCart();
+    })
+
 })
